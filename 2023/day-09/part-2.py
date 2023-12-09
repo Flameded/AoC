27f377a0559a96c69b1c3972e30a534405807879
Flameded <119843795+Flameded@users.noreply.github.com>
2023-12-09 19:43:12 +0000

    Create part-2.py

---
+++ 2023/day-09/part-2.py
@@ -0,0 +1,19 @@
+with open("input.txt", "r") as f:
+    inp = f.readlines()
+
+inp = [[int(x) for x in l.split()] for l in inp]
+
+t = 0
+for seq in inp:
+
+    sequences = [seq]
+    while any(seq):
+        seq = [seq[i + 1] - seq[i] for i in range(len(seq) - 1)]
+        sequences.append(seq)
+
+    x = 0
+    for s in sequences[::-1]:
+        x = s[0] - x
+    t += x
+
+print(f"Part 2: {t}")
